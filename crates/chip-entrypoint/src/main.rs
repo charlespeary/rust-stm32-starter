064869749c9133a26a082bdb5d2f1edd8abcbd35
gruszkakarol <karolgruszka9@gmail.com>
2025-03-01 17:32:55 +0100

    feat: basic stm32 example

--- crates/chip-entrypoint/src/main.rs
+++ crates/chip-entrypoint/src/main.rs
@@ -1,9 +1,15 @@
 #![no_std]
 #![no_main]
+use defmt::*;
+
+use embassy_executor::Spawner;
 use {defmt_rtt as _, panic_probe as _}; // global logger
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
+    let p = embassy_stm32::init(Default::default());
+    info!("Hello World!");
+
     // let p = embassy_nrf::init(Default::default());
 
     // let led = Output::new(p.P0_13, Level::Low, OutputDrive::Standard);
